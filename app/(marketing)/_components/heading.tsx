e886ee2fae5555f837599c4d28e4d2f5f865abec
Prophecyuntold <Aminurr999@gmail.com>
2023-11-15 07:13:24 +0000

    create group layouts: marketting, auth. and header component

---
+++ app/(marketing)/_components/heading.tsx
@@ -0,0 +1,15 @@
+"use client";
+import React from "react";
+
+const Heading = () => {
+  return (
+    <div className="max-w-3xl space-y-4">
+      <h1>
+        Your Ideas, Documents, & Plans. Unified. Welcome to{" "}
+        <span className="underline"> Motion </span>
+      </h1>
+    </div>
+  );
+};
+
+export default Heading;
