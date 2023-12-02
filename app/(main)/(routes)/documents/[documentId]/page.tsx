708bb37d2874c3e944a90797b6068cebb2d25e0d
Prophecyuntold <Aminurr999@gmail.com>
2023-12-02 15:49:50 +0000

    create cover component to display image

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -9,6 +9,7 @@ import { Id } from "@/convex/_generated/dataModel";
 import { Toolbar } from "@/components/toolbar";
 // import { Cover } from "@/components/cover";
 import { Skeleton } from "@/components/ui/skeleton";
+import { Cover } from "@/components/cover";
 
 interface DocumentIdPageProps {
   params: {
@@ -38,7 +39,7 @@ const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
   if (document === undefined) {
     return (
       <div>
-        {/* <Cover.Skeleton /> */}
+        <Cover.Skeleton />
         <div className="md:max-w-3xl lg:max-w-4xl mx-auto mt-10">
           <div className="space-y-4 pl-8 pt-4">
             <Skeleton className="h-14 w-[50%]" />
@@ -57,8 +58,7 @@ const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
 
   return (
     <div className="pb-40">
-      {/* <Cover url={document.coverImage} /> */}
-      <div className="h-[35vh]" />
+      <Cover url={document.coverImage} />
       <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
         <Toolbar initialData={document} />
         {/* <Editor onChange={onChange} initialContent={document.content} /> */}
